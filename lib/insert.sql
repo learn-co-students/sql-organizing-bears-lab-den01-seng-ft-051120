2c8bd495b4402bf325da417fb93ee93551b1359e
Rene Avila <reneavila1993@gmail.com>
2020-05-21 16:11:45 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+insert into bears(name, age, gender, color, temperament) values
+("Mr. Chocolate", 9, "M", "Brown", "Friendly");
+insert into bears(name, age, gender, color, temperament) values
+("Rowdy", 6, "F", "Brindle", "Rowdy");
+insert into bears(name, age, gender, color, temperament) values
+("Tabitha", 12, "F", "Black", "Chill");
+insert into bears(name, age, gender, color, temperament) values
+("Sergeant Brown", 16, "M", "Brown", "Mellow");
+insert into bears(name, age, gender, color, temperament) values
+("Melissa", 13, "F", "Black/White", "Aggressive");
+insert into bears(name, age, gender, color, temperament) values
+("Grinch", 7, "M", "Green", "Not Friendly");
+insert into bears(name, age, gender, color, temperament) values
+("Wendy", 3, "F", "Brown", "Playful");
+insert into bears(name, age, gender, color, temperament) values
+(Null, 6, "M", "Panda", "Aggressive");
