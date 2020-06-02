7b0cc3cdd7696acdb4d0a043e5709644c22e6895
matthewmjm <matthewmjm@yahoo.com>
2020-06-02 17:56:51 -0600

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,17 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "brown", "friendly", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "M", "black", "rowdy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "F", "black", "motherly", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 14, "M", "brown", "dominant", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "F", "brown", "curious", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 18, "M", "black", "mean", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "F", "black", "docile", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "M", "black", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 19, "M", "green", "slimy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "black", "grinchy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "blue", "naive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "M", "black", "aggressive", 0);
+
+-- (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+-- (2,"Rowdy", 10, "M", "black", "intense", 1),
+-- (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+-- (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+-- (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+-- (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+-- (7,"Wendy", 6, "F", "Blue", "naive", 1),
+-- (8,null, 20, "M", "black", "aggressive", 0);
